e8aa5e5158a17e4509174dde4e4046ac167eb8ba
Augtons <79037928+Augtons@users.noreply.github.com>
2023-06-17 00:35:32 +0800

    queue API 适配C++17 std::optional<>简化使用

--- examples/queue/main/queue.cpp
+++ examples/queue/main/queue.cpp
@@ -24,18 +24,26 @@ extern "C" void app_main()
     queue<Test> q(32);
 
     auto task1 = task_builder<void>("task1").stack(2048).priority(0).bind([=]() {
+        int count = 0;
         while (1) {
             q.send(Test());
             ESP_LOGI("TAG", "Sent! ref_count = %ld", q.use_count());  // It's 2, because of lambda capture.
             vTaskDelay(pdMS_TO_TICKS(1000));
+            if (++count > 5) {
+                return;
+            }
         }
     });
 
     while (1) {
-        auto v = q.receive_forever();
-        ESP_LOGI("TAG", "Received[%ld]: %s", q.use_count(), v.str.c_str());
-        ESP_LOGI("TAG", "Received[%ld]: %d", q.use_count(), v.a);
-        ESP_LOGI("TAG", "FreeHeap: %.3f M", (float)esp_get_free_heap_size() / 1024 / 1024);
+        auto v = q.receive(pdMS_TO_TICKS(5000));
+        if (v) {
+            ESP_LOGI("TAG", "Received[%ld]: %s", q.use_count(), v.value().str.c_str());
+            ESP_LOGI("TAG", "Received[%ld]: %d", q.use_count(), v.value().a);
+            ESP_LOGI("TAG", "FreeHeap: %.3f M", (float)esp_get_free_heap_size() / 1024 / 1024);
+        } else {
+            ESP_LOGI("TAG", "Received[%ld]: null", q.use_count());
+        }
     }
 
 }
